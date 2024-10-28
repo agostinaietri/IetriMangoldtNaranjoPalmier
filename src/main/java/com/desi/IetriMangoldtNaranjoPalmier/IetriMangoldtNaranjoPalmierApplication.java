4946c860473c799bfc55782a145b95b213e87024
Ronald Palmier <rpalmier@gmail.com>
2024-10-28 16:00:13 -0300

    + Prueba

---
+++ src/main/java/com/desi/IetriMangoldtNaranjoPalmier/IetriMangoldtNaranjoPalmierApplication.java
@@ -0,0 +1,13 @@
+package com.desi.IetriMangoldtNaranjoPalmier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IetriMangoldtNaranjoPalmierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IetriMangoldtNaranjoPalmierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desi/IetriMangoldtNaranjoPalmier/IetriMangoldtNaranjoPalmierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desi.IetriMangoldtNaranjoPalmier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IetriMangoldtNaranjoPalmierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
